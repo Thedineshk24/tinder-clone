cd622710be24383d4dbcfbde6c1b1c6f3e9ba2e6
Dinesh Katariya <thedineshk24@gmail.com>
2020-08-04 22:06:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text text-info"> tinder-clone </h2>
+
+      {/* {Header} */}
+      {/* {Tinder cards} */}
+      {/* {buttons below tinder cards} */}
+
+      {/* {chat screen} */}
+      {/* {individual Chat screen} */}
+    </div>
+  );
+}
+
+export default App;
