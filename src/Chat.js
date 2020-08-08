f52d594f793022546fee9cb6583cdde73b205db7
Dinesh Katariya <thedineshk24@gmail.com>
2020-08-08 22:34:00 +0530

    .

--- src/Chat.js
+++ src/Chat.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 import "./Chat.css";
 
 const Chat = ({ name, message, profilePic, timestamp }) => {
   return (
-    <div className="chat">
-      <Avatar className="chat_image" alt={name} src={profilePic} />
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <Avatar className="chat_image" alt={name} src={profilePic} />
 
-      <div className="chat_details">
-        <h3>{name}</h3>
-        <p>{message}</p>
+        <div className="chat_details">
+          <h3>{name}</h3>
+          <p>{message}</p>
+        </div>
+        <p className="chat_timestamp">{timestamp}</p>
       </div>
-      <p className="chat_timestamp">{timestamp}</p>
-    </div>
+    </Link>
   );
 };
 
--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -1,21 +1,50 @@
 import React, { useState } from "react";
+import Avatar from "@material-ui/core/Avatar";
 
+import "./chatScreen.css";
+// NOTE : ADDED 'ID' TO REMOVE KEY WARNING
 const ChatScreen = () => {
   const [message, setMessage] = useState([
     {
       name: "Anushka",
-      image: "...",
+      image:
+        "https://www.freewalldownload.com/anushka-shetty/amazing-anushka-shetty-home-look-free-desktop-mobile-background-hd-wallpaper.jpg",
       message: "hey! dinesh how are u?",
+      id: 1,
     },
     {
-      name: "Kajal",
-      image: "...",
+      name: "Anushka",
+      image:
+        "https://www.freewalldownload.com/anushka-shetty/amazing-anushka-shetty-home-look-free-desktop-mobile-background-hd-wallpaper.jpg",
       message: "Hi Dinesh ❤",
+      id: 2,
+    },
+    {
+      message: "Hey How Are u Anushka",
+      id: 3,
     },
   ]);
   return (
     <div className="chatScreen">
-      <h2>ChatScreen</h2>
+      <p className="chatScreen_timestamp">
+        You Matched With Anushka on 8/8/2020
+      </p>
+      {message.map((msg) =>
+        msg.name ? (
+          <div className="chatScreen_message" key={msg.id}>
+            <Avatar
+              className="chatScreen_avatar"
+              alt={msg.name}
+              src={msg.image}
+            />
+            <p className="chatScreen_text"> {msg.message} </p>
+          </div>
+        ) : (
+          <div className="chatScreen_message" key={msg.id}>
+            <p className="chatScreen_textUser"> {msg.message} </p>
+          </div>
+        )
+      )}
     </div>
   );
 };
